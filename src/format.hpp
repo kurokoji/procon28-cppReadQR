7ebb7ae601c507179f3185253591bdfd3bd31ad8
kurokoji <shu.kakihana@gmail.com>
2017-07-18 01:14:26 +0900

    boostのsplit使わず自前実装
    ヒント用のformat追加

--- src/format.hpp
+++ src/format.hpp
@@ -7,24 +7,38 @@
 #include <string>
 #include <vector>
 
-#include "boost/algorithm/string.hpp"
-
-namespace ba = boost::algorithm;
-
 namespace util {
-std::string format(std::string& s) {
+std::vector<std::string> split(const std::string& s, char delim) {
+  std::vector<std::string> ret;
+  std::stringstream ss(s);
+  std::string token;
+
+  while (std::getline(ss, token, delim)) {
+    ret.emplace_back(token);
+  }
+
+  return ret;
+}
+
+// 問題を受け取るとき
+std::string problem_format(std::string& s) {
   // コロンの数(データ数)
   const size_t sep_N = std::count(std::begin(s), std::end(s), ':');
   std::ostringstream ret;
   std::vector<std::string> split_s;
 
   std::replace(std::begin(s), std::end(s), ':', ' ');
-  ba::split(split_s, s, boost::is_any_of(" "));
+
+  split_s = split(s, ' ');
 
   // ピース数
   const size_t pieces_N = std::stoi(split_s[0]);
   // フレームのホール数
   const size_t holes_N = sep_N - pieces_N;
+
+  if (holes_N == 0) {
+    throw "Exception : wrong format\n";
+  }
 
   ret << pieces_N << '\n';
 
@@ -50,6 +64,36 @@ std::string format(std::string& s) {
       ret << split_s[index + j] << " " << split_s[index + j + 1] << '\n';
     }
     index += point_N * 2;
+  }
+
+
+  return ret.str();
+}
+
+// ヒントを受け取るとき
+std::string hint_format(std::string& s) {
+  std::ostringstream ret;
+
+  std::replace(std::begin(s), std::end(s), ':', ' ');
+  std::vector<std::string> split_s = split(s, ' ');
+
+  // ピース数
+  const size_t pieces_N = std::stoi(split_s[0]);
+  ret << pieces_N << '\n';
+
+  size_t index = 1;
+  for (size_t i = 0; i < pieces_N; ++i) {
+    // 各ピースの頂点数
+    const size_t point_N = std::stoi(split_s[index]);
+    ret << split_s[index++] << '\n';
+    for (size_t j = 0; j < point_N * 2; j += 2) {
+      ret << split_s[index + j] << " " << split_s[index + j + 1] << '\n';
+    }
+    index += point_N * 2;
+  }
+
+  if (index != split_s.size()) {
+    throw "Exception : wrong format\n";
   }
 
   return ret.str();
